d29e8db76f062adb9911030a05ad1aa5210926fa
ICA109 <t.yin1@outlook.com>
2018-02-23 00:32:54 -0500

    Simple tic-tac-toe game

---
+++ ticTacToe.py
@@ -0,0 +1,30 @@
+gameBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+        
+#Function to print current board status
+def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+
+turn = 'X'
+for i in range(9):
+    printBoard(gameBoard)
+    print('Turn for ' + turn + '. Move on which space?')
+    move = input()
+    
+    while (move not in gameBoard.keys() or (gameBoard[move] != ' ' )):
+        print('Error invalid move. Try again.')
+        move = input()
+    
+    gameBoard[move] = turn
+    if (turn == 'X'):
+        turn = 'O'
+    else:
+        turn = 'X'
+
+printBoard(gameBoard)
